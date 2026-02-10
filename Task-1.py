91f6596bfe21f043e5b80aa2ad0ead7e791151ce
Adwait-Mahajan12 <mahajanadwait0@gmail.com>
2026-02-11 00:59:42 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number = "))
+
+# to check number is odd or even
+
+if(num % 2 ==0):
+    print(f"{num} is an even numberan .")
+
+else:
+    print(f"{num} is an odd number.")
